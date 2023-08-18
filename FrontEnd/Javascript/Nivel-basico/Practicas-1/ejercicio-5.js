1daa3e76b2f2d1fad965c86d7c093635a8de3431
Dilan Espinoza <moreno475@gmail.com>
2023-08-18 00:50:53 -0500

    first commit

---
+++ FrontEnd/Javascript/Nivel-basico/Practicas-1/ejercicio-5.js
@@ -0,0 +1,3 @@
+/* 
+5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+*/
---
+++ FrontEnd/Javascript/Nivel-basico/Practicas-3/ejercicio-15.js
@@ -0,0 +1,3 @@
+/* 
+15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.
+*/
