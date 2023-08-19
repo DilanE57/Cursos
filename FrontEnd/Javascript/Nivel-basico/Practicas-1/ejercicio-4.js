8bd79a7a08074b817c3ab9f97adddcff5e2891d0
Dilan Espinoza <moreno475@gmail.com>
2023-08-19 18:14:39 -0500

    completando las practicaas-1

--- FrontEnd/Javascript/Nivel-basico/Practicas-1/ejercicio-4.js
+++ FrontEnd/Javascript/Nivel-basico/Practicas-1/ejercicio-4.js
@@ -1,3 +1,10 @@
 /* 
-4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+✅
 */
+
+function miFuncion(texto, repetir) {
+	return texto.repeat(repetir);
+}
+
+console.log(miFuncion("Hola Mundo ", 3));
--- FrontEnd/Javascript/Nivel-basico/Practicas-1/ejercicio-5.js
+++ FrontEnd/Javascript/Nivel-basico/Practicas-1/ejercicio-5.js
@@ -1,3 +1,10 @@
 /* 
 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+✅
 */
+
+const miFuncion = (texto) => {
+	return texto.split("").reverse().join("");
+};
+
+console.log(miFuncion("Hola Mundo"));
